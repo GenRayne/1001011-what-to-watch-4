7ee9e092ff25ae881275665fb1cf21b95980ddfb
Инна Подистова <gengakutaku@ya.ru>
2020-06-13 16:42:20 +0700

    Изменен интерфейс App и Main, добавлены PropTypes, генерация карточек с названиями фильмов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {arrayOf, shape, string} from 'prop-types';
 import Main from '../main/main.jsx';
 
-// eslint-disable-next-line react/prop-types
-const App = ({title, genre, releaseYear}) => (
+const App = ({movie, movies}) => (
   <Main
-    title={title}
-    genre={genre}
-    releaseYear={releaseYear}
+    movie={movie}
+    movies={movies}
   />
 );
+
+App.propTypes = {
+  movie: shape({
+    title: string.isRequired,
+    genre: string.isRequired,
+    releaseYear: string.isRequired,
+  }),
+  movies: arrayOf(
+      shape({
+        title: string.isRequired
+      })
+  ),
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
-const MovieData = {
-  TITLE: `The Grand Budapest Hotel`,
-  GENRE: `Drama`,
-  RELEASE_YEAR: `2014`,
+const MOVIES = [
+  {title: `Fantastic Beasts`},
+  {title: `Bohemian Rhapsody`},
+  {title: `Macbeth`}
+];
+
+const someMovie = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  releaseYear: `2014`,
 };
 
 ReactDOM.render(
     <App
-      title={MovieData.TITLE}
-      genre={MovieData.GENRE}
-      releaseYear={MovieData.RELEASE_YEAR}
+      movie={someMovie}
+      movies={MOVIES}
     />,
     document.querySelector(`#root`)
 );
